60b5847caa51e98cdcb7fd12728b053f0f0c7207
Babitha Shetty K <babitha.shetty@globaldelight.com>
2021-11-02 16:53:35 +0530

    add one

---
+++ myFramework/myFramework/mySwiftClass.swift
@@ -0,0 +1,13 @@
+//
+//  mySwiftClass.swift
+//  myFramework
+//
+//  Created by Babitha Shetty K on 02/11/21.
+//
+
+import Foundation
+public class myTest{
+    public class func one(){
+        print("onr")
+    }
+}
